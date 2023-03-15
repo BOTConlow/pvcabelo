a09bd5bb215322cf9ff02cc3e9420d75615fe71b
Victor <uskenmellorios@gmail.com>
2023-03-15 14:13:52 -0300

    meu commit

--- src/components/whatsapp/styles.js
+++ src/components/whatsapp/styles.js
@@ -8,7 +8,7 @@ export const WhatsappContent = styled.div`
     z-index: 99;
     border-radius: 50px;
     padding: .4rem;
-    background: linear-gradient(0deg, ${props => props.theme.secondary}, ${props => props.theme.third});
+    background: linear-gradient(0deg, #5eeb34, #49b828);
 
     @media (max-width: 1000px) {
             right: 1rem;
