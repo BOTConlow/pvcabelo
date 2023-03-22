a2cbba0b8c89a5f048c4c6ddad481ddc303869d2
Victor <uskenmellorios@gmail.com>
2023-03-22 08:16:44 -0300

    commit

--- src/pages/home/components/vsl/index.jsx
+++ src/pages/home/components/vsl/index.jsx
@@ -12,16 +12,17 @@ export function Vsl() {
                     <h1>Como Faço Uma <span style={{ color: '#fadabb' }}>Hidratação Poderosa</span> No Chuveiro Que <span style={{ color: '#fadabb' }}>Hidrata</span> e <span style={{ color: '#fadabb' }}>Potencializa</span> o <span style={{ color: '#fadabb' }}>Crescimento.</span></h1>
                 </Headline>
                 <VslStyle>
+
                     <iframe
                         id="panda-428d0b21-6183-4a1d-aced-409d81ed9325"
                         src="https://player-vz-5d275861-3c8.tv.pandavideo.com.br/embed/?v=428d0b21-6183-4a1d-aced-409d81ed9325"
                         style={{ border: 'none' }}
                         allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture"
-                        width="100%"
-                        height="100%"
+                        width="720"
+                        height="360"
                         fetchpriority="high">
-
                     </iframe>
+
                     <p>ATENÇÃO: Esse vídeo poderá sair do ar a qualquer momento</p>
                 </VslStyle>
             </Content>
