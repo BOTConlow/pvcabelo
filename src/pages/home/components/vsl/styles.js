6501676a7d5ffee8e36039453379ce417592c2da
Victor <uskenmellorios@gmail.com>
2023-03-22 08:57:08 -0300

    commit

--- src/pages/home/components/vsl/styles.js
+++ src/pages/home/components/vsl/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const VslContent = styled.div`
 
-    height: 710px;
+    height: 800px;
     padding: 3rem 0;
     display: flex;
     flex-direction: column;
